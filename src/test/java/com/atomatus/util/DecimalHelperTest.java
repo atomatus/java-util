1b292504a27d008deb212a929a0d7be587ce3691
Carlos Matos <chcmatos@hotmail.com>
2020-12-14 21:07:13 -0300

    Fix scale error on DecimalHelper

--- src/test/java/com/atomatus/util/DecimalHelperTest.java
+++ src/test/java/com/atomatus/util/DecimalHelperTest.java
@@ -36,11 +36,5 @@ public class DecimalHelperTest extends TestCase {
                 DecimalHelper.toBigDecimal(BigInteger.valueOf(Integer.MAX_VALUE)),
                 DecimalHelper.toBigDecimal(Integer.MAX_VALUE),
                 DecimalHelper.toBigDecimal((long) Integer.MAX_VALUE));
-
-         long cents = DecimalHelper.toCentsAsLong(fromStr);
-         assertEquals(135467L, cents);
-         BigDecimal cash = DecimalHelper.fromCents(cents);
-         assertEquals(cash, fromStr);
-
     }
 }
